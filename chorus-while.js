e5359a23454dd611aff4a70e7ff0a827c3c515da
Russell M <russell.e.mair@gmail.com>
2021-10-16 14:46:41 +0000

    While Loop testing

---
+++ chorus-while.js
@@ -0,0 +1,18 @@
+const chorus = "Just dance! Let the record play";
+let repeat = 0;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("It's gonna be ok.");
+
+const refrain = "Corpus Christi";
+let test = 0;
+while (test < 10) {
+  if (test === 5) {
+    console.log("*change key*");
+  }
+  console.log(refrain);
+  test++;
+}
+console.log("Deus Ex");
